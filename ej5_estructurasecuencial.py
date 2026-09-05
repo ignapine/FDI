d22d4d87b72bed3e7805c2cdcbd3c54300907415
ignacio.pineyro <nacho.pineyro.26@gmail.com>
2026-09-04 21:13:00 -0300

    ejercicios de estructuras

---
+++ ej5_estructurasecuencial.py
@@ -0,0 +1,39 @@
+#ejercicio 1 
+#print("hola mundo") ejercicio 1 
+#ejercicio 2
+#numero1 = int(input ("poner un numero")) ejercicio 2 
+#numero2 = int(input("poner otro numero"))
+#print("sumados dan ",numero1+numero2)
+#ejercicio 3 
+#numero = 5
+#factorial = 1
+#for i in range(1, numero + 1):
+#    factorial *= i
+#
+#print(factorial)
+#ejercicio 4
+#celsius = float(input("Ingrese la temperatura en Celsius: "))
+#
+#fahrenheit = (celsius * 9/5) + 32
+#ejercicio 5
+#print("Temperatura en Fahrenheit:", fahrenheit)
+#altura = float(input("ingrese la altura del rectangulo"))
+#ancho = float(input("ingrese el ancho del rectangulo"))
+#area = ancho*altura 
+#print("el area del rectangulo es ",area)
+# ejercicio 6 
+#num1 = float(input("Digame un numero: "))
+#
+#num2 = float(input("Ingrese otro numero: "))
+#
+#num3 = float(input("Ingrese un ultimo numero: "))
+#
+#if num3 > num1 and num3 > num2:
+#    print("El numero 3 es el mas grande de los 3")
+#
+#if num2 > num1 and num2 > num3:
+#    print("El numero 2 es el mas grande de los 3")
+#
+#if num1 > num2 and num1 > num3:
+#    print("El numero 1 es el mas grande de los 3")
+
