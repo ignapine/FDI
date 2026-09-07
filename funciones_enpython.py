6bc977fc605738e3e1ced0aece20644059c4d5f5
ignacio.pineyro <nacho.pineyro.26@gmail.com>
2026-09-06 21:46:03 -0300

    definiciones

--- funciones_enpython.py
+++ funciones_enpython.py
@@ -1,10 +1,19 @@
-def sumar (a,b):
-    resultado = a+b
-    return (a+b)
+#def sumar (a,b):
+#    resultado = a+b
+#    return (a+b)
+#
+#resultado = sumar(10,10)
+#
+#primt("la suma de 10 mas 10 es" +(resultado))
+#
 
-resultado = sumar(10,10)
 
-primt("la suma de 10 mas 10 es" +(resultado))
+import math
 
+area =float(input("dame un valor del area"))
 
+def area_circulo(radio):
+    area = math.pi * radio ** 2
+    return area
 
+print(area_circulo(area))
